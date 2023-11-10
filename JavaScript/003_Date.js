7f19118474901d579069e246c8e076b8e5d47647
shubham singh <43708880+shubh5900@users.noreply.github.com>
2023-11-10 17:23:01 +0000

    Date time js

---
+++ JavaScript/003_Date.js
@@ -0,0 +1,14 @@
+let myDate=new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// // let myCreateDate=new Date(2023,0,23)
+// let myCreateDate =new Date("10/11/2023")
+// console.log(myCreateDate.toDateString());
+
+let myTimeStamp=Date.now()
+
+console.log(myTimeStamp)
